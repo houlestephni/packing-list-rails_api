0ddbac8c1375d2b86f0b4f33e9236e1379984c1e
Stephni Houle <stephnihoule@stephnis-air.gateway.pace.com>
2019-12-20 01:08:40 -0500

    added destroy route for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :destroy]
-  resources :lists, only: [:index, :show, :create] do
+  resources :lists, only: [:index, :show, :create, :destroy] do
     resources :items, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
